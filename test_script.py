1956ffc94d59cb0c2f9942b3a8fb02f3e6d59c58
Adam Ottvar <adam.ottvar@gmail.com>
2019-08-06 12:43:27 +0200

    New looks combined with a authentication.

--- test_script.py
+++ test_script.py
@@ -1,7 +1,13 @@
+import os
 import threading
 import math
 from time import sleep, localtime, strftime
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash, session
+from passlib.hash import sha256_crypt
+
+# dummy hash for 'admin' and 'password'
+username_hash = '$5$rounds=535000$CWrew0XEF3Z2MHZV$5ly.8fTwUhxW48rrp70NVw.TIdV0X.jZVwGCErlsRq5'
+password_hash = '$5$rounds=535000$pE43ViC/UiLBakSo$0HXlVz5HO3thZ1y3Jf8QDX7YE4GeVEW49Wae8z2m.SB'
 
 logfile = "log.txt"
 
@@ -12,21 +18,50 @@ wind = 12.4
 temp_max = -999.9
 temp_min = 999.9
 
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.method == 'POST' and 'resetBtn' in request.form:
+        reset_values()
+    
+    elif request.method == 'POST' and 'updateBtn' in request.form:
+        index()
+    
+    elif request.method == 'POST' and 'loginBtn' in request.form:
+        username_correct = sha256_crypt.verify(request.form['username'], username_hash)
+        password_correct = sha256_crypt.verify(request.form['password'], password_hash)
+        if username_correct  and password_correct:
+            session['logged_in'] = True
+        else:
+            print("Wrong credentials entered")
+
+    elif request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+    
+    else:
+        flash('wrong password!')
+    
+    return index()
+
 @app.route('/', methods=['GET','POST'])
 @app.route('/index', methods=['GET','POST'])
 def index():
-    global rainfall, wind, temp_max, temp_min
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        global temp_max, temp_min
 
+        temp = round(23.6578443333, 1)
+
+        temp_max = max(temp_max, temp)
+        temp_min = min(temp_min, temp)
+        return render_template('index.html', temp = temp, wind = wind, rainfall = rainfall, temp_max = temp_max, temp_min = temp_min)
+
+def reset_values():
+    global rainfall, temp_max, temp_min
     temp = round(23.6578443333, 1)
-
-    if request.method == 'POST':
-        rainfall = 0
-        temp_max = temp
-        temp_min = temp
-
-    temp_max = max(temp_max, temp)
-    temp_min = min(temp_min, temp)
-    return render_template('index.html', temp = temp, wind = wind, rainfall = rainfall, temp_max = temp_max, temp_min = temp_min)
+    rainfall = 0
+    temp_max = temp
+    temp_min = temp
 
 def init_logging():
     with open(logfile, "w") as text_file:
@@ -43,8 +78,7 @@ def log_values():
     threading.Timer(30.0, log_values).start()
 
 if __name__ == '__main__':
-
     # To start logging values
     #init_logging()
-
+    app.secret_key = os.urandom(12)
     app.run(host='0.0.0.0')
